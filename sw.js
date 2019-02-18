791706696b09ddbb4bb84914c0693efabdf1727c
John.Wang <souer_w@163.com>
2019-02-18 20:41:39 +1100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,60 +1,69 @@
+let staticCacheName = 'restaurant-cache';
+let urlsToCache = [
+  './',
+  './index.html',
+  './restaurant.html',
+  './sw.js',
+  './css/styles.css',
+  './js/main.js',
+  './js/restaurant_info.js',
+  './js/dbhelper.js',
+  './data/restaurants.json',
+  './img/1.jpg',
+  './img/2.jpg',
+  './img/3.jpg',
+  './img/4.jpg',
+  './img/5.jpg',
+  './img/6.jpg',
+  './img/7.jpg',
+  './img/8.jpg',
+  './img/9.jpg',
+  './img/10.jpg',
+];
+
 self.addEventListener('install', function(event) {
-    event.waitUntil(
-      caches.open("restaurantMap").then(function(cache) {
-        return cache.addAll([
-          '/',
-          'index.html',
-          'restaurant.html',
-          'css/styles.css',
-          'js/dbhelper.js',
-          'js/main.js',
-          'js/restaurant_info.js',
-          'img/1.jpg',
-          'img/2.jpg',
-          'img/3.jpg',
-          'img/4.jpg',
-          'img/5.jpg',
-          'img/6.jpg',
-          'img/7.jpg',
-          'img/8.jpg',
-          'img/9.jpg',
-          'img/10.jpg'
-        ]);
-      })
+	event.waitUntil(
+		caches.open(staticCacheName).then(function(cache) {
+			return cache.addAll(urlsToCache);
+        })
     );
-  });
-  
-  self.addEventListener('activate', function(event) {
+});
+
+self.addEventListener('activate', function(event) {
     event.waitUntil(
-      caches.keys().then(function(cacheNames) {
-        return Promise.all(
-          cacheNames.filter(function(cacheName) {
-            return cacheName === "restaurantMap";
-          }).map(function(cacheName) {
-            return caches.delete(cacheName);
-          })    
-        );
-      })
+        caches.keys().then(function(cacheNames) {
+            return Promise.all(
+                cacheNames.filter(function(cacheName) {
+                    return cacheName.startsWith('restaurant-') &&
+                        cacheName != staticCacheName;
+                }).map(function(cacheName) {
+                    return caches.delete(cacheName);
+                })
+            );
+        })
     );
-  });
-  
-  self.addEventListener('fetch', (event) => {
-    event.respondWith(
-      caches.match(event.request).then(response => {
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
         if (response) {
           return response;
         }
-        return fetch(event.request).then(networkResponse => {
-          if (networkResponse.status === 404) {
-            return null;
+        return fetch(event.request).then(
+          function(response) {
+            if(!response || response.status !== 200 || response.type !== 'basic') {
+              return response;
+            }
+            var responseToCache = response.clone();
+            caches.open(staticCacheName)
+              .then(function(cache) {
+                cache.put(event.request, responseToCache);
+              });
+            return response;
           }
-          return caches.open("restaurantMap").then(cache => {
-            cache.put(event.request.url, networkResponse.clone());
-            return networkResponse;
-          })
-        })
-      }).catch(error => {
-        return;
+        );
       })
     );
-  });
+});
