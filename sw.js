fdf76b0e70b1fed2e2b8a7d9cc7261c9328dcebc
John.Wang <souer_w@163.com>
2019-02-21 21:29:28 +1100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-let staticCacheName = 'restaurant-cache';
+let CACHE_NAME = 'restaurant-cache';
 let urlsToCache = [
   './',
   './index.html',
@@ -18,52 +18,67 @@ let urlsToCache = [
   './img/7.jpg',
   './img/8.jpg',
   './img/9.jpg',
-  './img/10.jpg',
+  './img/10.jpg'
 ];
 
 self.addEventListener('install', function(event) {
-	event.waitUntil(
-		caches.open(staticCacheName).then(function(cache) {
-			return cache.addAll(urlsToCache);
-        })
-    );
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
 });
 
-self.addEventListener('activate', function(event) {
-    event.waitUntil(
-        caches.keys().then(function(cacheNames) {
-            return Promise.all(
-                cacheNames.filter(function(cacheName) {
-                    return cacheName.startsWith('restaurant-') &&
-                        cacheName != staticCacheName;
-                }).map(function(cacheName) {
-                    return caches.delete(cacheName);
-                })
-            );
-        })
-    );
+self.addEventListener('activate', event => {
+  // delete any caches that aren't in expectedCaches
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys.map(key => {
+        if (!urlsToCache.includes(key)) {
+          return caches.delete(key);
+        }
+      })
+    )).then(() => {
+      console.log('new verson cache now ready to handle fetches!');
+    })
+  );
 });
+
 
 self.addEventListener('fetch', function(event) {
   event.respondWith(
     caches.match(event.request)
       .then(function(response) {
+        // Cache hit - return response
         if (response) {
+          console.log('response found in cache');
           return response;
         }
-        return fetch(event.request).then(
+
+        var fetchRequest = event.request.clone();
+
+        return fetch(fetchRequest).then(
           function(response) {
+            // Check if we received a valid response
             if(!response || response.status !== 200 || response.type !== 'basic') {
               return response;
             }
+
             var responseToCache = response.clone();
-            caches.open(staticCacheName)
-              .then(function(cache) {
+
+            caches.open(CACHE_NAME).then(function(cache) {
                 cache.put(event.request, responseToCache);
-              });
+                console.log(event.request + ' is added to cache');
+            });
+
             return response;
           }
         );
-      })
-    );
+        return fetch(event.request);
+      }
+    )
+  );
 });
